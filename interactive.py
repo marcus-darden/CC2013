0e782c8f1b3f70d688e85e6bbf74c8805ad18eb8
Marcus M. Darden <ontoral@gmail.com>
2013-12-09 01:33:10 -0500

    Seed some simple data for testing in interactive mode.

--- interactive.py
+++ interactive.py
@@ -1,2 +1,25 @@
 from CC2013 import app, db, oid, lm
 from CC2013.models import *
+
+ont = User.query.get(1)
+
+if ont and ont.nickname == 'Ontoral':
+    csm = Program.query.get(1)
+else:
+    ont = User()
+    ont.nickname = 'Ontoral'
+    ont.email = 'ontoral@gmail.com'
+    db.session.add(ont)
+    db.session.commit()
+
+    csm = Program(ont, 'CS Major [2014]', 'The Computer Science Major')
+    db.session.add(csm)
+    db.session.commit()
+
+    cs1 = Course(csm, 'Computer Science I', 'CS 140', 'The introduction to Computer Science')
+    db.session.add(cs1)
+    cs2 = Course(csm, 'Computer Science II', 'CS 240', 'The introduction, continued.')
+    db.session.add(cs2)
+    dm = Course(csm, 'Discrete Mathematics', 'MTH 242')
+    db.session.add(dm)
+    db.session.commit()
