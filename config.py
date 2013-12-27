20c9766b9fe25d52ef3eeff234c9c5b4fa5aa848
Marcus M. Darden <ontoral@gmail.com>
2013-12-27 16:52:00 -0500

    Use environment variable for db URL if available.

--- config.py
+++ config.py
@@ -12,7 +12,8 @@ CSRF_ENABLED = True
 SECRET_KEY = 'Computing Curricula 2013 Webapp'
 
 # Database config
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',
+                                         'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 WHOOSH_BASE = os.path.join(basedir, 'search.db')
 SQLALCHEMY_RECORD_QUERIES = True
