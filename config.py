ae2bdd5f55446175790b55c1a391f5de833bb396
Marcus M. Darden <ontoral@gmail.com>
2013-12-29 23:55:48 -0500

    Extend db timeout.

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 WHOOSH_BASE = os.path.join(basedir, 'search.db')
 WHOOSH_ENABLED = bool(os.environ.get('HEROKU'))
 SQLALCHEMY_RECORD_QUERIES = True
-DATABASE_QUERY_TIMEOUT = 0.5  # slow database query threshold (in seconds)
+DATABASE_QUERY_TIMEOUT = 5  # slow database query threshold (in seconds)
 
 # OpenID config
 OPENID_PROVIDERS = [
